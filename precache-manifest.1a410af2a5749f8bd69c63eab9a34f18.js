5acd5c65327330310a33e4e621885023aa66be80
sannbala24 <sannbala24@gmail.com>
2020-10-01 01:28:30 +0530

    Updates

---
+++ precache-manifest.1a410af2a5749f8bd69c63eab9a34f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62dcfce61ffa50a128b14444c911323",
+    "url": "/spiderIndia/index.html"
+  },
+  {
+    "revision": "6d309f68bed3d2a6ae1b",
+    "url": "/spiderIndia/static/css/main.5af733e9.chunk.css"
+  },
+  {
+    "revision": "7831b1c6c3f409ae3392",
+    "url": "/spiderIndia/static/js/2.cdd2e068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spiderIndia/static/js/2.cdd2e068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d309f68bed3d2a6ae1b",
+    "url": "/spiderIndia/static/js/main.87ae403f.chunk.js"
+  },
+  {
+    "revision": "e0b267dc024be625fd2d",
+    "url": "/spiderIndia/static/js/runtime-main.df92e87c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spiderIndia/static/media/logo.5d5d9eef.svg"
+  }
+]);
